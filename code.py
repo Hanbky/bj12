af4ea0756a6db0dac665f96bc53d7372bc926f4e
Hanbky <704477912@qq.com>
2017-09-19 22:39:13 +0800

    增加视图index函数

--- code.py
+++ code.py
@@ -2,3 +2,6 @@ from django.db import models
 from django.http import HttpReponse
 def view(request):
     return HttpResponse('hello python')
+
+def index(request):
+    return HttpResponse('index')
